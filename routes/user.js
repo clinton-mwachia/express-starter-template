e9f0bfaef388ba9944249504a000001f316bf76b
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-20 11:39:00 +0300

    added a function to register a user

---
+++ routes/user.js
@@ -0,0 +1,34 @@
+/**
+ * users routes
+ * http://localhost:4050/api/v0/user
+ */
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+/**
+ * defining the router
+ */
+const router = express.Router();
+
+/**
+ * register user
+ */
+router.post("/register", async (req, res) => {
+  try {
+    let obj = new User(req.body);
+    obj.password = bcrypt.hashSync(req.body.password, 10);
+    const user = await obj.save();
+    if (!user) {
+      return res.status(400).json({ message: "cannot register new user" });
+    } else {
+      return res.send(user);
+    }
+  } catch (error) {
+    return res.status(500).json({ status: "fail", message: error.message });
+  }
+});
+
+module.exports = router;
