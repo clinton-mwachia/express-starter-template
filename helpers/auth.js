acea335d8f83d6caf032bb93a0b52da1395bcd26
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-11-21 11:16:36 +0300

    adding authentication

---
+++ helpers/auth.js
@@ -0,0 +1,19 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+function auth() {
+  const secret = process.env.SECRET;
+  const API = process.env.API;
+  return jwt({
+    secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+      `${API}/user/login`,
+      `${API}/user/login/`,
+      `${API}/user/register`,
+      `${API}/user/register/`,
+    ],
+  });
+}
+
+module.exports = auth;
